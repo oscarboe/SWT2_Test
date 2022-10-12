97d9cb2ccaefe7ad49dc635cf70d800a0ad4567a
oscarboe <Oscarboen@gmail.com>
2022-10-12 15:42:55 +0200

    Add project files.

---
+++ SWT2_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SWT2_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
